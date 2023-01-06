07bd3ea2124a374d991722c7c166b8abb9dc0670
OneTooMany <santosismael.115@gmail.com>
2023-01-05 16:13:29 -0800

    Add event listener to div, that when 'mouseover' will change color

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -10,3 +10,12 @@ function createGrid(x) {
         container.appendChild(div);
     }
 } createGrid(256);
+
+//function that when user hovers over div grid with mouse, the color changes, leaving behind a trail
+
+const div = document.querySelectorAll('.grids');
+div.forEach ((div) => {
+    div.addEventListener('mouseover', () => {
+        div.style['background'] = 'white';
+    })
+})
